2292da2b636c189481cc6d0cddcc46873956fb1b
AlphaBoom <rsshinide38@163.com>
2017-07-01 23:56:07 +0800

    Multiple lights

--- LearnOpenGL/LearnOpenGL/lightTest.cpp
+++ LearnOpenGL/LearnOpenGL/lightTest.cpp
@@ -137,6 +137,14 @@ int main()
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
+	// positions of the point lights
+	glm::vec3 pointLightPositions[] = {
+		glm::vec3(0.7f,  0.2f,  2.0f),
+		glm::vec3(2.3f, -3.3f, -4.0f),
+		glm::vec3(-4.0f,  2.0f, -12.0f),
+		glm::vec3(0.0f,  0.0f, -3.0f)
+	};
+
 	unsigned int VBO, cubeVAO;
 	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &VBO);
@@ -170,12 +178,6 @@ int main()
 	lightingShader.use();
 	lightingShader.setInt("material.diffuse",0);
 	lightingShader.setInt("material.specular", 1);
-	lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-	lightingShader.setFloat("material.shininess", 32.0f);
-
-	lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-	lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-	lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 	
 	
 
@@ -204,21 +206,63 @@ int main()
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("light.position",camera.Position);
 		lightingShader.setVec3("viewPos", camera.Position);
-
-		// light properties
-		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-
-		lightingShader.setVec3("light.position", camera.Position);
-		lightingShader.setVec3("light.direction", camera.Front);
-		lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
-		lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
-		// material properties
-		lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
 		lightingShader.setFloat("material.shininess", 32.0f);
+
+		/*
+		Here we set all the uniforms for the 5 / 6 types of lights we have.We have to set them manually and index
+			the proper PointLight struct in the array to set each uniform variable.This can be done more code - friendly
+			by defining light types as classes and set their values in there, or by using a more efficient uniform approach
+			by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
+		*/
+		// directional light
+		lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+		lightingShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+		lightingShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+		// point light 1
+		lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
+		lightingShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[0].constant", 1.0f);
+		lightingShader.setFloat("pointLights[0].linear", 0.09);
+		lightingShader.setFloat("pointLights[0].quadratic", 0.032);
+		// point light 2
+		lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
+		lightingShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[1].constant", 1.0f);
+		lightingShader.setFloat("pointLights[1].linear", 0.09);
+		lightingShader.setFloat("pointLights[1].quadratic", 0.032);
+		// point light 3
+		lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
+		lightingShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[2].constant", 1.0f);
+		lightingShader.setFloat("pointLights[2].linear", 0.09);
+		lightingShader.setFloat("pointLights[2].quadratic", 0.032);
+		// point light 4
+		lightingShader.setVec3("pointLights[3].position", pointLightPositions[3]);
+		lightingShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[3].constant", 1.0f);
+		lightingShader.setFloat("pointLights[3].linear", 0.09);
+		lightingShader.setFloat("pointLights[3].quadratic", 0.032);
+		// spotLight
+		lightingShader.setVec3("spotLight.position", camera.Position);
+		lightingShader.setVec3("spotLight.direction", camera.Front);
+		lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+		lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("spotLight.constant", 1.0f);
+		lightingShader.setFloat("spotLight.linear", 0.09);
+		lightingShader.setFloat("spotLight.quadratic", 0.032);
+		lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+		lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 		
 
 		// view/projection transformations
@@ -227,6 +271,10 @@ int main()
 		lightingShader.setMat4("projection", projection);
 		lightingShader.setMat4("view", view);
 
+
+		// world transformation
+		glm::mat4 model;
+		lightingShader.setMat4("model", model);
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, diffuseMap);
@@ -251,14 +299,16 @@ int main()
 		lampShader.use();
 		lampShader.setMat4("projection", projection);
 		lampShader.setMat4("view", view);
-		glm::mat4 model;
-		model = glm::mat4();
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		lampShader.setMat4("model", model);
 
 		glBindVertexArray(lightVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 4; i++)
+		{
+			model = glm::mat4();
+			model = glm::translate(model, pointLightPositions[i]);
+			model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+			lampShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 
 
--- LearnOpenGL/LearnOpenGL/lighting.fs
+++ LearnOpenGL/LearnOpenGL/lighting.fs
@@ -9,19 +9,43 @@ struct Material {
     float shininess;
 }; 
 
-struct Light {
-	vec3 position;
+struct DirLight{
 	vec3 direction;
 
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+};
+
+struct PointLight{
+	vec3 position;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+struct SpotLight{
+	vec3 position;
 
 	float cutOff;
 	float outerCutOff;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specualr;
 };
 
-uniform Light light;
+#define NR_POINT_LIGHTS 4
+
+uniform DirLight dirLight;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+uniform SpotLight spotLight;
+
 uniform Material material;
 uniform vec3 viewPos;
 
@@ -30,37 +54,52 @@ in vec3 Normal;
 in vec2 TexCoords;
 
 
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light,vec3 normal,vec3 fragPos,vec3 viewDir);
+vec3 CalcSpotLight();
+
 void main()
 {
-	// 环境光
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	//属性
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
 
-    // 漫反射 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords));
+	//平行光
+	vec3 result = CalcDirLight(dirLight, norm , viewDir);
+	//点光源
+	for(int i = 0; i < NR_POINT_LIGHTS; i++)
+		result += CalcPointLight(pointLights[i],norm,FragPos,viewDir);
 
-    // 镜面光
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));  
+	FragColor = vec4(result,1.0);
 
-	float theta = dot(lightDir, normalize(-light.direction));
-	float epsilon = light.cutOff - light.outerCutOff;
-	float intensity = clamp((theta - light.outerCutOff)/epsilon, 0.0,1.0);
+}
 
-	diffuse *= intensity;
-	specular *= intensity;
+vec3 CalcDirLight(DirLight light,vec3 normal,vec3 viewDir)
+{
+	vec3 lightDir = normalize(-light.direction);
+	float diff = max(dot(normal,lightDir),0.0);
+	vec3 reflectDir = reflect(-lightDir,normal);
+	float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
 
-	if(theta > light.outerCutOff)
-	{
-		vec3 result = ambient + diffuse + specular;
-		FragColor = vec4(result, 1.0);
-	}
-	else
-	{
-		FragColor = vec4(ambient,1.0);
-	}
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+	return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light,vec3 normal,vec3 fragPos,vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	float diff = max(dot(normal, lightDir),0.0);
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
+	float distance = length(light.position - fragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
+	vec3 diffuse = light.diffuse * vec3(texture(material.diffuse,TexCoords));
+	vec3 specular = light.specular * vec3(texture(material.specular,TexCoords));
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+	return (ambient + diffuse + specular);
 }
