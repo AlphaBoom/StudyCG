4964c8dc11c696c46a383ba72916d895bb09a312
AlphaBoom <rsshinide38@163.com>
2017-08-01 21:44:08 +0800

    sprite render

--- BreakUp/BreakUp/game.cpp
+++ BreakUp/BreakUp/game.cpp
@@ -1,5 +1,9 @@
 #include "game.h"
 #include "resource_manager.h"
+#include "sprite_renderer.h"
+
+
+SpriteRenderer *Renderer;
 
 Game::Game(GLuint width, GLuint height)
 	:State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -12,9 +16,18 @@ Game::~Game()
 
 }
 
+
 void Game::Init()
 {
+	ResourceManager::LoadShader("resources/shaders/sprite.vs", "resources/shaders/sprite.fs", nullptr, "sprite");
 
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+
+	Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+
+	ResourceManager::LoadTexture("resources/textures/awesomeface.png", GL_TRUE, "face");
 }
 
 void Game::Update(GLfloat dt)
@@ -29,5 +42,5 @@ void Game::ProcessInput(GLfloat dt)
 
 void Game::Render()
 {
-
+	Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
