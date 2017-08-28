908886f8efb48040f06f926ba5f61712e4aa2e79
AlphaBoom <rsshinide38@163.com>
2017-08-29 00:43:32 +0800

    move inlcudes lib to git root directory

--- BreakOut/BreakOut/game.cpp
+++ BreakOut/BreakOut/game.cpp
@@ -8,7 +8,7 @@ using namespace irrklang;
 #include "sprite_renderer.h"
 #include "game_object.h"
 #include "ball_object.h"
-#include "particle_generator.h"
+#include "particle_generator.cpp"
 #include "post_processor.h"
 
 
